a496a3a11651f941b4c33abf67c81eac7f3f049c
Sara <sara.tankard@gmail.com>
2025-03-13 15:33:34 -0400

    Updating order instructions

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -74,29 +74,21 @@ export default function Menu() {
 
       <h1 style={{ marginTop: "30px" }}>How to Order</h1>
       <div className={styles.lower_container}>
-        <ol className={styles.order_list}>
+        <ul className={styles.order_list}>
           <li>
-            Send me a message via{" "}
-            <a href="https://ig.me/m/the.twisted.taco/ ">instagram</a> to place
-            your order with
-            <ul style={{ marginLeft: "25px" }}>
-              <li className={styles.sublistitem}>Your name</li>
-              <li className={styles.sublistitem}>Your order</li>
-              <li className={styles.sublistitem}>
-                Payment type (cash, venmo, zelle)
-              </li>
-            </ul>
-            <p className={styles.screenshot}>
-              If using venmo or zelle, please include a screenshot of payment
-              confirmation. <span>🙏</span>
-            </p>
+            Fill out an order form <br />{" "}
+            <i>
+              There should be order forms on your table or with us at order
+              station table in the back.
+            </i>
           </li>
-
-          <li>When your order is ready, I will send you message.</li>
-          <li>Come to the pick up table to get your tacos!</li>
-          <li>Enjoy!</li>
-        </ol>
-        {/*<p className={styles.payment}>If you are paying with cash, we will collect payment when you pick up your order at the table.</p> */}
+          <li>Please fill out 1 form for each person/plate.</li>
+          <li>
+            Bring the order form(s) to the order station table in the back to
+            pay & submit your order. We accept cash/zelle/venmo
+          </li>
+          <li>When your order is ready, we will bring it to your table.</li>
+        </ul>
 
         <div>
           <p className={styles.payment_option}>
