a0281dcf23c2b90377d6ea728113cad1ccacca94
Sara <sara.tankard@gmail.com>
2025-03-20 16:28:17 -0400

    Fri Mar 21 menu

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -36,18 +36,35 @@ export default function Menu() {
       description:
         "Grilled steak marinated with mexican spices topped with pico de gallo and queso fresco",
       imageUri: "/steak.jpeg",
+      show: false,
     },
     {
       title: "Pork Carnitas",
       description:
         "Slow cooked pork lion marinated with pineapple and mexican spices topped with onions, cilantro and queso fresco on top",
       imageUri: "/pork2.jpeg",
+      show: true,
     },
     {
       title: "Cauliflower",
       description:
         "Fried cauliflower on a flour tortilla topped with purple cabbage salad, queso fresco and chipotle mayo.",
       imageUri: "/cauliflower.jpeg",
+      show: false,
+    },
+    {
+      title: "Cajun Shrimp",
+      description:
+        "Spicy, pan seared shrimp in a soft flour tortilla bursting with cajun flavors. Topped with shredded lettuce and pico de gallo.",
+      imageUri: "/shrimp.png",
+      show: true,
+    },
+    {
+      title: "Grilled Chicken",
+      description:
+        "Grilled herb marinated chicken with melted cheddar cheese. Topped with pico de gallo, lettuce and cilantro avocado salsa on top.",
+      imageUri: "/chicken.png",
+      show: true,
     },
   ];
 
@@ -92,7 +109,7 @@ export default function Menu() {
 
       <h1>Menu</h1>
       <div className={styles.menuContainer}>
-        {MenuItems.map((item) => (
+        {MenuItems.filter((item) => item.show).map((item) => (
           <MenuItem
             key={item.title}
             title={item.title}
@@ -108,6 +125,14 @@ export default function Menu() {
         <h2>$18 for large plate</h2>
         <h3>(3 tacos)</h3>
       </div>
+      <h4 style={{ marginTop: "7px" }}>
+        Quesadillas available for kids ($8) <br />
+      </h4>
+      <p>
+        <br />
+        *We also can prepare vegetarian tacos. <br /> If interested, please
+        inquire when you order.
+      </p>
 
       <h1 style={{ marginTop: "30px" }}>How to Order</h1>
       <div className={styles.lower_container}>
@@ -121,9 +146,18 @@ export default function Menu() {
           </li>
           <li>Please fill out 1 form for each person/plate.</li>
           <li>
-            Bring the order form(s) to the order station table in the back to
-            pay & submit your order. We accept cash/zelle/venmo
+            Bring the order form(s) to the order station table in the back bar
+            by the water station.{" "}
+            <i>(On the right before you get to the bathrooms)</i>
+            <br />
+            <br /> We will give to a table number and collect payment. We accept
+            cash/zelle/venmo.
+            <br />
+            <br />
+            If no one is there, feel free to leave your order in the basket and
+            take a table number. We will be back shortly
           </li>
+
           <li>When your order is ready, we will bring it to your table.</li>
         </ul>
 
